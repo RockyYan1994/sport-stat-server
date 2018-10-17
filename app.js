cbee73a6e22561ee1905119ab199dac5f677d8a3
jucai <jucai@github.com>
2018-10-16 18:27:36 -0700

    ADD: [10.16] add /test as test API

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 var app = express();
 
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
     res.send("Hi, Siri!");
+});
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the home page");
 });
 
 app.listen(port, () => {
