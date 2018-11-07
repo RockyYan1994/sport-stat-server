2391bdf40d0efff3d66162de85db4ce54a18b2f1
jucai <jucai@github.com>
2018-11-06 17:07:02 -0800

    ADD: [11.06] use sport_stat_db

--- db/sport-stat-db.js
+++ db/sport-stat-db.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
     host     : 'sport-stat-db.ce5yfibisle8.us-west-1.rds.amazonaws.com',
     user     : 'sportStatMaster',
     password : 'lgnblgnb',
-    database : 'player_info_spider'
+    database : 'sport_stat_db'
     //database : 'sport_stat_db'
   });
 
